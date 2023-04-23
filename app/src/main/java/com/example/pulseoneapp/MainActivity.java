df5be24a11a881a840d87b0c825b07bed18dd636
Hanzker99 <113298614+Hanzker99@users.noreply.github.com>
2023-04-24 01:48:19 +0300

    24Apr2023/01:38

--- app/src/main/java/com/example/pulseoneapp/MainActivity.java
+++ app/src/main/java/com/example/pulseoneapp/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.pulseoneapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.button.MaterialButton;
 
@@ -28,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
                 if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
                     //corect
                     Toast.makeText(MainActivity.this, "Login", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, SeocndActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
                 } else 
                     //incorect
                     Toast.makeText(MainActivity.this, "Not login", Toast.LENGTH_SHORT).show();
