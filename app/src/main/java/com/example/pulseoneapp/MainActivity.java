243879d718b7fa8150420520e10f8b341cf64f12
Hanzker99 <113298614+Hanzker99@users.noreply.github.com>
2023-04-24 01:39:08 +0300

    24Apr2023/01:38

--- app/src/main/java/com/example/pulseoneapp/MainActivity.java
+++ app/src/main/java/com/example/pulseoneapp/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.pulseoneapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView username =(TextView) findViewById(R.id.username);
+        TextView password =(TextView) findViewById(R.id.password);
+
+        MaterialButton loginbtn =(MaterialButton) findViewById(R.id.loginbtn);
+
+        //admin
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
+                    //corect
+                    Toast.makeText(MainActivity.this, "Login", Toast.LENGTH_SHORT).show();
+                } else 
+                    //incorect
+                    Toast.makeText(MainActivity.this, "Not login", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
